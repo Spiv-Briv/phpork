c3809528e3a91b20b8eb4c909c2a3cc9e82b1725
Dawid-Bebenek <72632697+Dawid-Bebenek@users.noreply.github.com>
2024-04-22 21:55:50 +0200

    PORK-6

--- routes/index.php
+++ routes/index.php
@@ -1,8 +1,40 @@
 <?php
 
-use App\Models\Country;
 use App\Models\Team;
 
 require_once "./../boot.php";
-
-echo Team::getAll();
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cope</title>
+</head>
+<body>
+    <?= Team::between("id", "14", "16"); ?>
+    <Container class="container"></Container>
+    <input id="page" type="number" />
+    <button onclick="getItem()">Załaduj</button>
+    <script>
+        let result;
+        function getItem() {
+            const container = document.getElementsByClassName('container')[0];
+            const page = document.getElementById("page").value;
+            result = fetch(`http://localhost/phpork/routes/api/index.php?page=${page}`, {method: "POST"}).then(response => {
+                console.log(response);
+                return response.json()
+            });
+            result.then(data => {
+                console.log(data);
+                container.innerHTML = "";
+                for(const item of data.data) {
+                    const div = document.createElement("div");
+                    div.innerText = `${item.id}. ${item.sponsor} ${item.city}`;
+                    container.appendChild(div);
+                }
+            });
+        }
+    </script>
+</body>
+</html>
